28adc72fdc4538bcb68259983f77be25f261625c
Nadji_Tan <nadjitan@yahoo.com.ph>
2020-09-19 19:59:52 +0800

    css changes and fixed the sign up conditions

--- js/custom/forms.js
+++ js/custom/forms.js
@@ -15,6 +15,9 @@ var stored_loggedin
 
 var account_found = false
 
+var regexUsername = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/
+var regexPassword = /[ ]/
+
 function GetInputValue() {
   this.stored_accounts = JSON.parse(localStorage.getItem('accounts')) || [] /* Retrieve */
   this.stored_loggedin = JSON.parse(localStorage.getItem('loggedin')) || [] /* Retrieve */
@@ -24,30 +27,43 @@ function GetInputValue() {
     password = document.getElementById("password").value
 
     InputNewAccount(username, password)
-    alert("Your are now registered.")
   }
 
   if (sPage == "log_in.html"){
     username = document.getElementById("username").value
     password = document.getElementById("password").value
 
-    SearchAccounts(username, password)
-
+    SearchAccounts(username, password)  
+    
     localStorage.setItem('loggedin', JSON.stringify(currently_loggedin)) || []  /* Save */
-    location.reload()
   }
 }
 
-function InputNewAccount(name, pass){
-  var accounts = {un: name, pw: pass}
+function ChangePage() {
+  window.location.pathname ='../index.html'
+} 
 
-  stored_accounts.push(accounts)
+function InputNewAccount(name, pass){
+  var notValidUsername = regexUsername.test(name);
+  var notValidPassword = regexPassword.test(pass);
+
+  if (name == "" || pass == "") {
+    alert(`Please complete form.`)
+  } 
+
+  if (notValidUsername || notValidPassword) {
+    alert(`Username or password not valid.`)
+  } 
   
-  localStorage.setItem('accounts', JSON.stringify(stored_accounts)) || []  /* Save */
+  if (!notValidUsername && !notValidPassword && name != "" && pass != "") {
+    var accounts = {un: name, pw: pass}
+    stored_accounts.push(accounts)
+    localStorage.setItem('accounts', JSON.stringify(stored_accounts)) || []  /* Save */
+    alert(`You are now registered.`)
+  }
 }
 
 function SearchAccounts(name, pass){
-
   try{
     for (let i = 0; i <= this.stored_accounts.length; i++) {
       if (name == stored_accounts[i].un && pass == stored_accounts[i].pw){
@@ -65,7 +81,9 @@ function SearchAccounts(name, pass){
   if (this.account_found == true){
     currently_loggedin.username = this.username
     currently_loggedin.password = this.password
+    
     alert("You are now logged in.")
+    ChangePage()
   }
-
+  
 }   
